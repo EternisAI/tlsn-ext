279fe6f362c39a23f6d02a20d126eaf98dc696f3
asa93 <asa93@live.fr>
2024-08-23 19:04:19 -0400

    add provider screen

---
+++ src/reducers/bookmarks.ts
@@ -0,0 +1,61 @@
+import { db } from '../entries/Background/db';
+import { RequestHistory } from '../entries/Background/rpc';
+import { sha256 } from '../utils/misc';
+
+export class BookmarkManager {
+  historyDb = db.sublevel<string, RequestHistory>('history', {
+    valueEncoding: 'json',
+  });
+
+  async getBookmarkIds(): Promise<string[]> {
+    const bookmarksId = await sha256('bookmarks');
+    try {
+      const storage = await localStorage.getItem(bookmarksId);
+      return storage ? JSON.parse(storage) : [];
+    } catch (e) {
+      return [];
+    }
+  }
+
+  async saveBookmarkIds(bookmarkIds: string[]): Promise<void> {
+    const bookmarksId = await sha256('bookmarks');
+    try {
+      await localStorage.setItem(bookmarksId, JSON.stringify(bookmarkIds));
+    } catch (e) {
+      console.error('Error saving bookmark IDs', e);
+    }
+  }
+
+  async addBookmarkId(bookmarkId: string): Promise<void> {
+    const bookmarkIds = await this.getBookmarkIds();
+    if (!bookmarkIds.includes(bookmarkId)) {
+      bookmarkIds.push(bookmarkId);
+      await this.saveBookmarkIds(bookmarkIds);
+    }
+  }
+
+  async addBookmark(request: RequestHistory) {
+    const id = await sha256(request.url);
+    await this.addBookmarkId(id);
+    await this.historyDb.put(id, request);
+  }
+
+  async getBookmark(id: string): Promise<RequestHistory | null> {
+    try {
+      const existing = await this.historyDb.get(id);
+      return existing;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  async getBookmarks(): Promise<RequestHistory[]> {
+    const bookmarkIds = await this.getBookmarkIds();
+    const bookmarks = await Promise.all(
+      bookmarkIds.map((id) => this.getBookmark(id)),
+    );
+    return bookmarks.filter(
+      (bookmark) => bookmark !== null,
+    ) as RequestHistory[];
+  }
+}

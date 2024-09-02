01c980df488f7d116f34172ac123c407628af2f4
asa93 <asa93@live.fr>
2024-09-02 18:25:24 -0400

    decode payload

--- src/remoteattestation/remoteattestation.ts
+++ src/remoteattestation/remoteattestation.ts
@@ -6,6 +6,23 @@ import * as cbor from 'cbor-web';
 
 const certPath = './cert.pem';
 
+interface RemoteAttestation {
+  payload: Buffer;
+  signature: Uint8Array;
+}
+
+interface Payload {
+  module_id: string;
+  timestamp: number;
+  digest: string;
+  pcrs: Uint8Array[];
+  certificate: Uint8Array;
+  cabundle: Uint8Array[];
+  public_key: Uint8Array | null;
+  user_data: Uint8Array | null;
+  nonce: Uint8Array | null;
+}
+
 function verifyCertificate(certPath: string, chainCerts: ChainCert[]) {
   // Read the certificate and CA certificate
 
@@ -31,33 +48,81 @@ function verifyCertificate(certPath: string, chainCerts: ChainCert[]) {
   return result;
 }
 
+function verifyES384Signature(publicKey: any, message: any, signature: any) {
+  try {
+    // Create a verifier object
+    const verifier = crypto.createVerify('SHA384');
+
+    // Add the message to be verified
+    verifier.update(message);
+
+    // Verify the signature
+    const isValid = verifier.verify(publicKey, signature, 'base64');
+
+    return isValid;
+  } catch (error) {
+    console.error('Error verifying signature:', error);
+    return false;
+  }
+}
+
+function decodeCbor(data: Buffer) {
+  try {
+    const decoded = cbor.decodeAllSync(data);
+
+    const remoteAttestation: RemoteAttestation = {
+      payload: decoded[0][2],
+      signature: new Uint8Array(decoded[0][3]),
+    };
+    return remoteAttestation;
+  } catch (e: any) {
+    console.log('Error decoding CBOR attestation', e.toString());
+    return null;
+  }
+}
+
+function decodeCborPayload(data: Buffer) {
+  try {
+    const decoded = cbor.decodeAllSync(data);
+    console.log(decoded);
+    const payload = decoded[0] as Payload;
+    return payload;
+  } catch (e: any) {
+    console.log('Error decoding CBOR payload', e.toString());
+    return undefined;
+  }
+}
+
+//fetch attestation
+// add axios code here..
+
+//parse cbor structure
+const base64String = fs.readFileSync('./remote_attestation', 'utf8');
+const remote_attestation_uint8 = Buffer.from(base64String, 'base64');
+const remote_attestation = decodeCbor(remote_attestation_uint8);
+
+if (remote_attestation) {
+  const payload = decodeCborPayload(remote_attestation.payload);
+  console.log(payload);
+}
+
+//verify signature
+const publicKey = `-----BEGIN PUBLIC KEY-----
+MHYwEAYHKoZIzj0CAQYFK4EEACIDYgAEXtIZwd2RFvOX+0YxccGAnzyzL4dN6Rb6
+WiZJxVIRdgHWCIALp5CASV7H+rvwlPG6P+TsFXzcZ+MoTkFLkF2WiF9vdv/IDnYw
+tC5tAYBgtJmEEZmZEFoSV5MXqvTfzUKC
+-----END PUBLIC KEY-----`;
+
+const message = 'Hello, world!';
+const signature =
+  'MEUCIQCvj5ZAMhm1/XMEQ6fCMj3jfRQyy8VjagL7h1SihLZiygIgDbL+hJtxZ1X7MThvvPPUBB/3eL6YHwcOF6FXKTsCj6g=';
+
+const isValid = verifyES384Signature(publicKey, message, signature);
+console.log('Signature is valid:', isValid);
+
+//verify x509 certificate
 // if (verifyCertificate(certPath, chainCerts)) {
 //   console.log('====\n End Certificate is valid 🟢');
 // } else {
 //   console.log('====\n End Certificate verification failed 🚫');
 // }
-
-interface RemoteAttestation {
-  payload: Uint8Array;
-  signature: Uint8Array;
-}
-
-function decodeCbor() {
-  try {
-    const base64String = fs.readFileSync('./remote_attestation', 'utf8');
-    const remote_attestation_uint8 = Buffer.from(base64String, 'base64');
-    const decoded = cbor.decodeAllSync(remote_attestation_uint8);
-
-    const remoteAttestation: RemoteAttestation = {
-      payload: new Uint8Array(decoded[0][2]),
-      signature: new Uint8Array(decoded[0][3]),
-    };
-    return remoteAttestation;
-  } catch (e: any) {
-    console.log('Error decoding CBOR data', e.toString());
-    return null;
-  }
-}
-
-const decoded = decodeCbor();
-console.log(decoded);

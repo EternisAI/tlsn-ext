5803e5d171117f850b8a66eee2e06668d27c8e70
asa93 <asa93@live.fr>
2024-09-14 19:50:14 -0400

    fix remote att badge design with v0

--- src/components/RemoteAttestationBadge/index.tsx
+++ src/components/RemoteAttestationBadge/index.tsx
@@ -8,6 +8,8 @@ import React, {
   useState,
 } from 'react';
 
+import { CheckCircle, XCircle } from 'lucide-react';
+
 import { useRemoteAttestation } from '../../reducers/remote-attestation';
 import Icon from '../Icon';
 export default function RemoteAttestationBadge(): ReactElement {
@@ -16,24 +18,45 @@ export default function RemoteAttestationBadge(): ReactElement {
   if (isValid === null) return <></>;
   return (
     <>
-      {isValid ? (
-        <span>🟢 Notary Authenticated</span>
+      <NotaryBadge isAuthenticated={isValid} />
+    </>
+  );
+}
+
+interface NotaryBadgeProps {
+  isAuthenticated: boolean;
+}
+
+export function NotaryBadge({ isAuthenticated = false }: NotaryBadgeProps) {
+  return (
+    <div
+      className={`inline-flex items-center px-3 py-1 rounded-full shadow-sm transition-all ${
+        isAuthenticated
+          ? 'bg-green-100 text-green-800 hover:bg-green-200'
+          : 'bg-red-100 text-red-800 hover:bg-red-200'
+      }`}
+      title={
+        (isAuthenticated
+          ? 'Valid remote attestation ! '
+          : 'Invalid remoteattestation') +
+        `The remote attestation guarantees the
+    authenticity of the code running the notary. Click to learn more`
+      }
+    >
+      {isAuthenticated ? (
+        <CheckCircle className="w-4 h-4 mr-2" />
       ) : (
-        <span title={error}>🔴 Notary Not Authentified</span>
+        <XCircle className="w-4 h-4 mr-2" />
       )}
 
       <a
+        className="text-sm font-medium inherit"
         href="https://aws.amazon.com/blogs/compute/validating-attestation-documents-produced-by-aws-nitro-enclaves/"
         target="_blank"
-        title={
-          (isValid
-            ? 'Valid remote attestation ! '
-            : 'Invalid remoteattestation') +
-          `The remote attestation guarantees the
-      authenticity of the code running the notary. Click to learn more`
-        }
         style={{ color: 'black', textDecoration: 'none' }}
-      ></a>
-    </>
+      >
+        {isAuthenticated ? 'Notary Authenticated' : 'Not Authenticated'}
+      </a>
+    </div>
   );
 }

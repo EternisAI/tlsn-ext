db6a5974f39017541eab14b8b3314244cb3cdedc
asa93 <asa93@live.fr>
2024-10-08 18:37:35 -0400

    add new websockify url from k8s

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,7 @@ export const MAX_RECV = 16384;
 export const MAX_SENT = 4096;
 
 export const NOTARY_API = 'https://notary.eternis.ai';
-export const NOTARY_PROXY = 'wss://inn1.eternis.ai:55688';
+export const NOTARY_PROXY = 'wss://websockify.cluster.eternis.ai:55688';
 
 export const NOTARY_API_LOCAL = 'http://localhost:7047';
 export const NOTARY_PROXY_LOCAL = 'ws://localhost:55688';
